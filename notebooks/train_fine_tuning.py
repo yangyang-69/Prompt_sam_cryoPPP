d9c9cb9a281111bade60b443354bc109c3d31e85
Newgin-sam <newginsam@gmail.com>
2023-11-10 14:54:07 -0600

    fine tuning test and train files push

---
+++ notebooks/train_fine_tuning.py
@@ -0,0 +1,140 @@
+import sys
+import os
+curPath = os.path.abspath(os.path.dirname(__file__))
+rootPath = os.path.split(curPath)[0]
+sys.path.append(rootPath)
+
+from tensorboardX import SummaryWriter
+from dataset import *
+from SAM_conf import settings
+from notebooks.SAM_conf import SAM_cfg
+from torch.utils.data import DataLoader
+from notebooks.SAM_conf.SAM_utils import *
+import function
+
+args = SAM_cfg.parse_args()
+
+GPUdevice = torch.device('cuda', args.gpu_device)
+
+net = get_network(args, args.net, use_gpu=args.gpu, gpu_device=GPUdevice, distribution=args.distributed)
+if args.pretrain:
+    weights = torch.load(args.pretrain)
+    net.load_state_dict(weights, strict=False)
+
+optimizer = optim.Adam(net.parameters(), lr=args.lr, betas=(0.9, 0.999), eps=1e-08, weight_decay=0, amsgrad=False)
+scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.5)  # learning rate decay
+
+'''load pretrained model'''
+if args.weights != 0:
+    print(f'=> resuming from {args.weights}')
+    assert os.path.exists(args.weights)
+    checkpoint_file = os.path.join(args.weights)
+    assert os.path.exists(checkpoint_file)
+    loc = 'cuda:{}'.format(args.gpu_device)
+    checkpoint = torch.load(checkpoint_file, map_location=loc)
+    start_epoch = checkpoint['epoch']
+    best_tol = checkpoint['best_tol']
+
+    net.load_state_dict(checkpoint['state_dict'], strict=False)
+    # optimizer.load_state_dict(checkpoint['optimizer'], strict=False)
+
+    args.path_helper = checkpoint['path_helper']
+    logger = create_logger(args.path_helper['log_path'])
+    print(f'=> loaded checkpoint {checkpoint_file} (epoch {start_epoch})')
+
+args.path_helper = set_log_dir('../logs', args.exp_name)
+logger = create_logger(args.path_helper['log_path'])
+logger.info(args)
+
+'''segmentation data'''
+transform_train = transforms.Compose([
+    transforms.Resize((args.image_size, args.image_size)),
+    transforms.ToTensor(),
+])
+
+transform_train_seg = transforms.Compose([
+    transforms.Resize((args.out_size, args.out_size)),
+    transforms.ToTensor(),
+])
+
+transform_test = transforms.Compose([
+    transforms.Resize((args.image_size, args.image_size)),
+    transforms.ToTensor(),
+])
+
+transform_test_seg = transforms.Compose([
+    transforms.Resize((args.out_size, args.out_size)),
+    transforms.ToTensor(),
+])
+
+if args.dataset == 'CryoPPP':
+    '''Cryoppp data'''
+    train_dataset = CryopppDataset(args, args.data_path, transform=transform_train,
+                                   transform_msk=transform_train_seg, mode='train', prompt=args.prompt_approach)
+    valid_dataset = CryopppDataset(args, args.data_path, transform=transform_test,
+                                   transform_msk=transform_test_seg, mode='valid', prompt=args.prompt_approach)
+
+    nice_train_loader = DataLoader(train_dataset, batch_size=args.b, shuffle=True, num_workers=8, pin_memory=True)
+    nice_valid_loader = DataLoader(valid_dataset, batch_size=args.b, shuffle=False, num_workers=8, pin_memory=True)
+    '''end'''
+
+elif args.dataset == 'decathlon':
+    nice_train_loader, nice_valid_loader, transform_train, transform_val, train_list, val_list = get_decath_loader(args)
+
+'''checkpoint path and tensorboard'''
+
+checkpoint_path = os.path.join(settings.CHECKPOINT_PATH, args.net, settings.TIME_NOW)
+
+if not os.path.exists(settings.LOG_DIR):
+    os.mkdir(settings.LOG_DIR)
+writer = SummaryWriter(log_dir=os.path.join(
+    settings.LOG_DIR, args.net, settings.TIME_NOW))
+
+# create checkpoint folder to save model
+if not os.path.exists(checkpoint_path):
+    os.makedirs(checkpoint_path)
+checkpoint_path = os.path.join(checkpoint_path, '{net}-{epoch}-{type}.pth')
+
+'''begin training'''
+best_acc = 0.0
+best_tol = 1e6
+best_iou = 1e10
+best_dice = 1e10
+for epoch in range(settings.EPOCH):
+    if args.mod == 'sam_fine':
+        net.train()
+        time_start = time.time()
+        loss = function.train_sam(args, net, optimizer, nice_train_loader, epoch, writer, vis=args.vis)
+        logger.info(f'Train loss: {loss}|| @ epoch {epoch}.')
+        time_end = time.time()
+        print('time_for_training ', time_end - time_start)
+
+        net.eval()
+        if epoch and epoch % args.val_freq == 0 or epoch == settings.EPOCH - 1:
+            tol, (eiou, edice) = function.validation_sam(args, nice_valid_loader, epoch, net, writer)
+            logger.info(f'Total score: {tol}, IOU: {eiou}, DICE: {edice} || @ epoch {epoch}.')
+
+            if args.distributed != 'none':
+                sd = net.module.state_dict()
+            else:
+                sd = net.state_dict()
+
+            if tol < best_tol or eiou > best_iou or edice > best_dice:
+                best_iou = eiou
+                best_dice = edice
+                best_tol = tol
+                is_best = True
+
+                save_checkpoint({
+                    'epoch': epoch + 1,
+                    'model': args.net,
+                    'state_dict': sd,
+                    'optimizer': optimizer.state_dict(),
+                    'best_tol': best_tol,
+                    'path_helper': args.path_helper,
+                }, is_best, args.path_helper['ckpt_path'], filename="best_checkpoint")
+                print("-------------save best checkpoint------------------")
+            else:
+                is_best = False
+
+writer.close()
