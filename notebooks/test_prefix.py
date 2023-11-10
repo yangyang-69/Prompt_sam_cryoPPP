77afff6c1995a23f20a6ee6b9c5dd7c8e5b1d015
yangyang-69 <yangyang9712280609@gmail.com>
2023-11-10 18:06:01 +0800

    no message

--- notebooks/test_prefix.py
+++ notebooks/test_prefix.py
@@ -1,3 +1,8 @@
+import sys
+import os
+curPath = os.path.abspath(os.path.dirname(__file__))
+rootPath = os.path.split(curPath)[0]
+sys.path.append(rootPath)
 
 from dataset import *
 from torch.utils.data import DataLoader
--- notebooks/train_head.py
+++ notebooks/train_head.py
@@ -1,3 +1,9 @@
+import sys
+import os
+curPath = os.path.abspath(os.path.dirname(__file__))
+rootPath = os.path.split(curPath)[0]
+sys.path.append(rootPath)
+
 import torch
 import torch.nn as nn
 import numpy as np
